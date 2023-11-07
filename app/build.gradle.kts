b07e421e95b343e53bbb77dabee0907e7a3db8ea
Andreas Selguson <andreas.selguson@saabgroup.com>
2023-11-07 10:59:00 +0100

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ma23_android_w1_lesson"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ma23_android_w1_lesson"
--- app/src/main/java/com/example/ma23_android_w1_lesson/MainActivity.kt
+++ app/src/main/java/com/example/ma23_android_w1_lesson/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.ma23_android_w1_lesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var count : Int = 0
+    lateinit var helloTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        helloTextView = findViewById(R.id.helloView)
+
+        helloTextView.text = "David"
 
 
-
+    }
+    fun changeText(){
+        helloTextView.text = "Susan"
     }
 }

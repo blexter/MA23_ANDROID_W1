c5d76a19b035d38134e35d51112d7583fe2495cc
Andreas Selguson <andreas.selguson@saabgroup.com>
2023-11-07 09:35:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ma23_android_w1_lesson/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ma23_android_w1_lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MA23_ANDROID_W1_LESSON"
+include(":app")
+ 
